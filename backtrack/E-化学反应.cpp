4403632feb376a7740c49ab6ae9e5a77bf47f195
smq <asongmingqing@qq.com>
2020-05-24 19:28:49 +0800

    add annotation for Ti.

--- backtrack/E-化学反应.cpp
+++ backtrack/E-化学反应.cpp
@@ -10,13 +10,16 @@ int toNum(char c){
 }
 
 void dfs(int u){
+    //如果找到了m,那么就返回
     if(u==toNum('m')){
         printf("Yes.\n");
         flag = 1;
         return;
     }
+    //遍历u能转换的物质
     for(int i=0;i<G[u].size();i++){
         int v = G[u][i];
+        //剪枝,前面已经转换过了,就不用转换了
         if(!vis[v]){
             vis[v] = true;
             dfs(v);
@@ -31,6 +34,7 @@ int main(){
     while (T--)
     {
         memset(vis,0,sizeof vis);
+        //是否能够转换
         flag = 0;
         for(int i=0;i<maxn;i++) G[i].clear();
         scanf("%d",&n);
@@ -38,9 +42,11 @@ int main(){
             char s[60];
             scanf("%s",s);
             int len = strlen(s);
+            //把字母a-z映射为0-25的数字
             int x = toNum(s[0]),y = toNum(s[len-1]);
             G[x].push_back(y);
         }
+        //从b开始转换
         dfs(toNum('b'));
         if(!flag) printf("No.\n");
     }
